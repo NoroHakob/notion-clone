68fff9b5225694757412473b527bc0ccfe319d89
NoroHakob <norohakob2004@gmail.com>
2026-05-05 16:02:03 +0400

    telegram part

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,24 +1,3 @@
-// import { Button } from "@/components/ui/button"
-// import { Logo } from "./logo"
-
-// export const Footer = () => {
-//     return (
-//         <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1c1a1a]">
-//             <Logo />
-//             <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-//                 <Button variant="ghost" size="sm">
-//                     Privacy Policy
-//                 </Button>
-//                 <Button variant="ghost" size="sm">
-//                     Terms & Conditions
-//                 </Button>
-//             </div>
-//         </div>
-//     )
-// }
-
-
-
 import { Button } from "@/components/ui/button"
 import { Logo } from "./logo"
 import { FaInstagram, FaTelegramPlane, FaFacebookMessenger, FaEnvelope } from "react-icons/fa"
